7874f6b9aee567c8113f2716f0f3f44fd8770ff1
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-08-20 08:01:04 +0300

    Completed Core Graphics Challenge

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -25,6 +25,8 @@ final class DetailViewController: UIViewController {
 
 		guard let imagePath = selectedImage else { return }
 		imageView.image = UIImage(named: imagePath)
+
+		addWaterMark()
 	}
 
 	override func viewWillAppear(_ animated: Bool) {
@@ -37,6 +39,7 @@ final class DetailViewController: UIViewController {
 		super.viewWillDisappear(animated)
 	}
 
+	// MARK: - Actions
 	@objc func shareTapped() {
 		guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
 			print("No image found")
@@ -46,5 +49,27 @@ final class DetailViewController: UIViewController {
 		let viewController = UIActivityViewController(activityItems: [image, selectedImage!], applicationActivities: [])
 		viewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
 		present(viewController, animated: true)
+	}
+
+	// MARK: - Image Processing
+	private func addWaterMark() {
+		guard let displayedImage = imageView.image else { return }
+		let imageSize = displayedImage.size
+		let renderer = UIGraphicsImageRenderer(size: imageSize)
+
+		let waterMarkedImage = renderer.image { context in
+			displayedImage.draw(at: imageView.frame.origin)
+
+			let attributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 32),
+															 .foregroundColor: UIColor.black.withAlphaComponent(0.6),
+															 .backgroundColor: UIColor.white.withAlphaComponent(0.6)]
+
+			let string = " From Storm Viewer "
+			let attributedString = NSAttributedString(string: string, attributes: attributes)
+
+			attributedString.draw(with: CGRect(x: 32, y: imageSize.height - 48, width: 448, height: 448), options: .usesLineFragmentOrigin, context: nil)
+		}
+
+		imageView.image = waterMarkedImage
 	}
 }
